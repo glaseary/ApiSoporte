6352b7798a2ce1a385a6a80c216eb514af0f0b7a
glaseary <sofia.troncoso.m@gmail.com>
2025-06-14 23:44:41 -0400

    primer commit

---
+++ src/main/java/com/Perfulandia/ApiSoporte/ApiSoporteApplication.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiSoporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSoporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSoporteApplication.class, args);
+	}
+
+}
